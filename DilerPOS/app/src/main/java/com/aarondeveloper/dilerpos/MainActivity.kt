15f324d45a15c3d7cacfb233f1b4b56e52dc1b06
Aaron Hernandez <126830445+aaron-developer25@users.noreply.github.com>
2024-10-30 19:35:09 -0400

    Se cargan los recursos del proyecto de forma inicial. Damos apertura al proyecto

---
+++ DilerPOS/app/src/main/java/com/aarondeveloper/dilerpos/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.aarondeveloper.dilerpos
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.aarondeveloper.dilerpos.ui.theme.DilerPOSTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DilerPOSTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    DilerPOSTheme {
+        Greeting("Android")
+    }
+}
