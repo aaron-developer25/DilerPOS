36f614ad719bb4d225ae3619d0975089cec455f6
Ruth2002Casilla <ruth_casilla@ucne.edu.do>
2024-11-01 19:32:08 -0400

    Agrega la clase VentaRepository

---
+++ DilerPOS/app/src/main/java/com/aarondeveloper/dilerpos/data/local/repository/VentaRepository.kt
@@ -0,0 +1,18 @@
+package com.aarondeveloper.dilerpos.data.local.repository
+
+import com.aarondeveloper.dilerpos.data.local.dao.VentaDao
+import com.aarondeveloper.dilerpos.data.local.entities.ventaEntity
+import javax.inject.Inject
+
+class VentaRepository @Inject constructor(
+    private val ventaDao: VentaDao
+){
+    suspend fun save(venta: ventaEntity) = ventaDao.save(venta)
+
+    suspend fun delete(venta: ventaEntity) = ventaDao.delete(venta)
+
+    suspend fun getVenta(id: Int) = ventaDao.find(id)
+
+    fun getAll() = ventaDao.getAll()
+
+}
